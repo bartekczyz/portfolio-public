49812a70e9288245c8b1da2b61fefca328d71a8c
Bartek Czyż <bartek@czyz.it>
2020-01-08 21:42:26 +0100

    Updates

---
+++ precache-manifest.8ae1a9b58aca6bb4c1be67c0642b4528.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe7ae0eaf80c1ddb6ac9892c04792a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e1863ca546f01a3103",
+    "url": "/static/css/main.37521bb3.chunk.css"
+  },
+  {
+    "revision": "22bcbf22bc6231f1d470",
+    "url": "/static/js/2.48e11a9d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.48e11a9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4e1863ca546f01a3103",
+    "url": "/static/js/main.ed0bd9f3.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "65d4a2fcf0c72cfc02777a78f363007e",
+    "url": "/static/media/background.65d4a2fc.jpg"
+  },
+  {
+    "revision": "cc2a62ec8180a2e42ab6099d29a6a1ae",
+    "url": "/static/media/smallBackground.cc2a62ec.svg"
+  }
+]);
